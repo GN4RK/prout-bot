fce0d9dc56582f117c5dc910009cd77b46e8135c
yoann.leonard <yoann.leonard@sewan.fr>
2026-03-10 15:36:20 +0100

    add getnowformat function

--- index.js
+++ index.js
@@ -103,6 +103,34 @@ function addLog(level, message, server = "", channel = "", author = "")
     console.log(JSON.stringify(infos));
 }
 
+function getNowFormat()
+{
+    const dateObj = new Date();
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth();
+    month = ('0' + (month + 1)).slice(-2);
+    // To make sure the month always has 2-character-format. For example, 1 => 01, 2 => 02
+
+    let date = dateObj.getDate();
+    date = ('0' + date).slice(-2);
+    // To make sure the date always has 2-character-format
+
+    let hour = dateObj.getHours();
+    hour = ('0' + hour).slice(-2);
+    // To make sure the hour always has 2-character-format
+
+    let minute = dateObj.getMinutes();
+    minute = ('0' + minute).slice(-2);
+    // To make sure the minute always has 2-character-format
+
+    let second = dateObj.getSeconds();
+    second = ('0' + second).slice(-2);
+    // To make sure the second always has 2-character-format
+
+    return `${year}-${month}-${date}T${hour}:${minute}:${second}`;
+}
+
+
 
 client.on('messageCreate', async function(message) {
     if (message.author.bot) return;
